6356994901b28c269b2d687e8b7b74617db3a16b
Muhammad Awais Naveed <awaisn872@gmail.com>
2026-03-18 16:38:44 +0500

    stop watch

---
+++ Stop Watch/app.js
@@ -0,0 +1,51 @@
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let timer = null;
+
+let displayTime = document.getElementById("displytime");
+let startBtn = document.getElementById("start");
+let stopBtn = document.getElementById("stop");
+let resetBtn = document.getElementById("reset");
+
+function runStopwatch() {
+    seconds++;
+
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = `${h}:${m}:${s}`;
+}
+
+
+startBtn.addEventListener("click", function () {
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+    timer = setInterval(runStopwatch, 1000);
+});
+
+
+stopBtn.addEventListener("click", function () {
+    clearInterval(timer);
+});
+
+
+resetBtn.addEventListener("click", function () {
+    clearInterval(timer);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    displayTime.innerHTML = "00:00:00";
+});
